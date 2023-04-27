120a3020bf42eda868a181b486f5d2d372bfea14
rahatsingh <131697605+rahatsingh@users.noreply.github.com>
2023-04-28 00:37:44 +0530

    Update app.py

--- exp-8/app.py
+++ exp-8/app.py
@@ -1,23 +1,20 @@
-from flask import *
+from flask import Flask, render_template, request
+
 
 app = Flask(__name__)
 
-
-@app.route("/form")
-def form():
-    return render_template('form.html')
-
-
-@app.route('/result/', methods = ['POST', 'GET'])
-def result():
-    if request.method == 'GET':
-        return f"The URL /result is accessed directly. Try going to '/form' to submit form"
-    if request.method == 'POST':
-        form_result = request.form
-        return render_template('result.html',r = form_result)
-
-
-if __name__ == "__main__":
-    app.run(debug=True)
-
+@app.route('/', methods=["GET", "POST"])
+def feedback():
+    if request.method == "POST":
+        first_name = request.form.get("fname")
+        L_name = request.form.get("lname")
+        M_name = request.form.get("mname")
+        email = request.form.get("email")
+        rating = request.form.get("star")
+        result_head = "Thank You "+first_name + " For Your feedback"
+        result_data = "Name:-"+first_name + " "+L_name+"Email: -"+email+" < br > "+"Rating: - "+rating
+        return " "+result_head+" <br> "+result_data
+    return render_template('index.html')
     
+if __name__=="__main__":
+    app.run(debug=True)
